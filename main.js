e2433299bb114a323c075a310e1368e9248e8dba
みやし <86143317+a-m-iyashi@users.noreply.github.com>
2021-11-01 11:36:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ $(function() {
     hamburger();
   });
   function hamburger() {
-    $('.l-header-hamburger').toggleClass('active');
+    $('.l-header-hamburger').toggleClass('active');    
     if ($('.l-header-hamburger').hasClass('active')) {  //l-header-hamburgerクラスにactiveクラスが存在する場合
       $('.l-header-spMenu').addClass('active');  //l-header-spMenuにもactiveクラスを追加する
     } else {  // hamburgerクラスにactiveクラスが存在しない場合
       $('.l-header-spMenu').removeClass('active');//l-header-spMenuからactiveクラスを削除する
     }
   }
+  $('#mask').on('click', function() {
+    $('.l-header-hamburger').removeClass('active');  
+  });
 });
