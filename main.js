3ce86da3c2672b1aecb25e15f7c22f08111f7b9a
みやし <86143317+a-m-iyashi@users.noreply.github.com>
2021-11-02 06:21:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -28,4 +28,20 @@ $(function() {
     });
   }
 });
+$(function(){
+  //  スムーススクロール
+  // ページ内のリンクをクリックした時に動作する
+  $('a[href^="#"]').click(function(){
+    // リンクを取得
+    let href= $(this).attr("href");
+    // ジャンプ先のid名をセット
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    // トップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う
+    // 500はスクロール速度で単位はミリ秒
+    $("html, body").animate({scrollTop:position}, 500, "swing");
+    return false;
+  });
+})
 
