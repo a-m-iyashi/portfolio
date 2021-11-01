e0a5b788291434e5fa09d3c860512171ed343bd9
みやし <86143317+a-m-iyashi@users.noreply.github.com>
2021-11-01 13:58:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,14 +10,21 @@ $(function() {
     } else {  // hamburgerクラスにactiveクラスが存在しない場合
       $('.l-header-spMenu').removeClass('active');//l-header-spMenuからactiveクラスを削除する
     }
-    if ($('#header').hasClass('open')) {
-      $('#header').removeClass('open');
+    if ($('.l-header-hamburger').hasClass('active')) {
+      $('.l-header').addClass('open');
     } else {
-      $('#header').addClass('open');
+      $('.l-header').removeClass('open');
     }
+    $('.l-header-spMenu__link').on('click', function() {
+      $('.l-header-hamburger').removeClass('active');
+      $('.l-header-spMenu').removeClass('active');
+      $('.l-header').removeClass('open');
+    });
     $('#mask').on('click', function() {
       // openクラスを削除して、メニューを閉じる
-      $('#footer').removeClass('open');
+      $('.l-header-hamburger').removeClass('active');
+      $('.l-header-spMenu').removeClass('active');
+      $('.l-header').removeClass('open');
     });
   }
 });
